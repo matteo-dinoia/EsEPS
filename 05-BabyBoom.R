b80a821f8bdc3410aad46f06d1925248eef913e8
Matteo <matteodinoia02@gmail.com>
2022-11-18 11:20:56 +0100

    adding last lesson exercise (on 05-BabyBoom.R)

--- 05-BabyBoom.R
+++ 05-BabyBoom.R
@@ -5,3 +5,35 @@ diff_born<-diff(babyboom$running.time)
 hist(babyboom$running.time)
 hist(diff_born, 40)
 sd(diff_born)/mean(diff_born)
+
+library(moments)
+data(package="moments")
+hist(babyboom$wt)
+boxplot(babyboom$wt, horizontal=TRUE)
+skewness(babyboom$wt)
+
+
+#BABIES
+data("babies")
+str(babies)
+hist(babies$inc[babies$inc<20], 10)
+
+babies$smoke_f<-as.factor(babies$smoke)
+levels(babies$smoke_f)<-c("mai", "sempre", "prima", "molto prima", "N/a")
+tab<-table(babies$smoke_f)/length(babies$smoke_f)
+barplot(tab)
+dotchart(tab)
+pie(tab, radius=1)
+
+#BIVARIATA wt e smoke
+
+#trasformo 999 in NA
+babies$wt[babies$wt==999]<-NA
+hist(babies$wt)
+#trasformo in grammi
+grams<-babies$wt*28.34952
+babies<-cbind(babies, grams)
+hist(babies$grams)
+
+boxplot(babies$grams/1000 ~ babies$smoke_f)
+summary(babies$grams/1000 ~ babies$smoke_f)
